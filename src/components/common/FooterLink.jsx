2280aee1904f1c4541eab6b70c6e3775a25e2008
amansiwach868 <amansiwach868@gmail.com>
2025-06-10 16:56:56 +0530

    changes

--- src/components/common/FooterLink.jsx
+++ src/components/common/FooterLink.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FooterLink = ({ linkName }) => {
   return (
-    <li className='sm:text-[12px] text-[8px] font-normal text-white leading-[100%]'><a href="#" target="_blank">{linkName}</a></li>
+    <li className='sm:text-[12px] text-[8px] font-normal text-white leading-[100%] overflow-x-clip'><a className=' after:h-[1px] after:w-full after:bg-white after:content-[""] relative after:absolute after:bottom-0 after:-left-full hover:after:left-0 after:duration-300 ease-in-out' href="#" target="_blank">{linkName}</a></li>
   )
 }
 
